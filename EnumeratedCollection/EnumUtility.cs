fd81be0fbdac8145f7ec908d2177ab4e8cf08c92
apilola <alec.pilola@gmail.com>
2024-05-21 11:26:35 -0400

    Added Summary Comments

--- EnumeratedCollection/EnumUtility.cs
+++ EnumeratedCollection/EnumUtility.cs
@@ -21,6 +21,13 @@ namespace AP.Utilities
         public static readonly bool IsContiguous;
         public static readonly int Offset;
 
+        /// <summary>
+        /// Validates that the enum is contiguous.
+        /// This is used to determine if the enum can be used as an index in an array.
+        /// </summary>
+        /// <returns>
+        /// Returns true if the enum is contiguous; otherwise, false.
+        /// </returns>
         public static bool ValidateIsContiguous()
         {
             for (int i = 1; i < Keys.Count; i++)
@@ -33,6 +40,14 @@ namespace AP.Utilities
             return true;
         }
 
+        /// <summary>
+        /// Returns an IEnumerable of the enum values between the start and end values.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns>
+        /// The IEnumerable that can be used to iterate over the enum values in the range.
+        /// </returns>
         public static IEnumerable<T> InRange(T start, T end)
         {
             var keys = EnumUtility<T>.Keys;
@@ -47,6 +62,22 @@ namespace AP.Utilities
 
     public static class EnumExtensions
     {
+        /// <summary>
+        /// Determines if the value is in the range of the start and end values.
+        /// This is inclusive of the start and end values.
+        /// </summary>
+        /// <param name="value">
+        /// The value to check.
+        /// </param>
+        /// <param name="start">
+        /// The start of the range.
+        /// </param>
+        /// <param name="end">
+        /// The end of the range.
+        /// </param>
+        /// <returns>
+        /// Returns true if the value is in the range; otherwise, false.
+        /// </returns>
         public static bool IsInRange(this Enum value, Enum start, Enum end)
         {
             var interger = (int)(object) value;
@@ -54,7 +85,12 @@ namespace AP.Utilities
         }
 
 
-
+        /// <summary>
+        /// Returns the a valid array index for the enum value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
         public static int AsCollectionIndex<T>(this T key) where T : Enum
         {
             return (int) (object) key - EnumUtility<T>.Offset;
--- EnumeratedCollection/EnumeratedCollection.cs
+++ EnumeratedCollection/EnumeratedCollection.cs
@@ -14,6 +14,9 @@ namespace AP.Collections
     [Serializable]
     public class EnumeratedCollection<TKey, TValue> : IEnumerable<TValue> where TKey : Enum
     {
+        /// <summary>
+        /// Gets the keys of the collection.
+        /// </summary>
         public ReadOnlyCollection<TKey> Keys => EnumUtility<TKey>.Keys;
 
 #if UNITY_2017_1_OR_NEWER
@@ -22,6 +25,10 @@ namespace AP.Collections
         private TValue[] _values;
         private bool _isReadOnly = false;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumeratedCollection{TKey, TValue}"/> class.
+        /// </summary>
+        /// <exception cref="InvalidOperationException"></exception>
         public EnumeratedCollection()
         {
             if (!EnumUtility<TKey>.IsContiguous) throw new InvalidOperationException($"Invalid Type Error: The type [{typeof(TKey)}] is not contiguous.");
@@ -33,6 +40,9 @@ namespace AP.Collections
             _values = new TValue[Keys.Count];
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumeratedCollection{TKey, TValue}"/> class.
+        /// <param name="defaultValue">The default value to initialize the collection with.</param>
         public EnumeratedCollection(TValue defaultValue) : this()
         {
             for (var i = 0; i < _values.Length; i++)
@@ -41,6 +51,12 @@ namespace AP.Collections
             }
         }
 
+        /// <summary>
+        /// Gets or sets the value associated with the specified key.
+        /// </summary>
+        /// <param name="enumIndex">The enum as a key to get or set the value.</param>
+        /// <returns>A value associated with the specified key.</returns>
+        /// <exception cref="InvalidOperationException">A InvalidOperationException is thrown if the collection is read-only.</exception>
         public TValue this[TKey enumIndex]
         {
             get => _values[enumIndex.AsCollectionIndex()];
@@ -51,6 +67,14 @@ namespace AP.Collections
             }
         }
 
+        /// <summary>
+        /// Sets the value at the specified index.
+        /// </summary>
+        /// <param name="index">
+        /// The index of the value to set.
+        /// </param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public TValue this[int index]
         {
             get => _values[index];
@@ -61,24 +85,58 @@ namespace AP.Collections
             }
         }
 
+        /// <summary>
+        /// Adds/Sets a key-value pair to the collection.
+        /// This method exists to allow the collection to be initialized with a collection initializer.
+        /// </summary>
+        /// <param name="key">
+        /// The key to add/set the value for.
+        /// </param>
+        /// <param name="value">
+        /// The value that is will be set.
+        /// </param>
+        /// <exception cref="InvalidOperationException"></exception>
         public void Add(TKey key, TValue value)
         {
             if (_isReadOnly) throw new InvalidOperationException("Collection is read-only.");
             this[key] = value;
         }
 
+        /// <summary>
+        /// The number of elements in the collection.
+        /// </summary>
         public int Count => _values.Length;
 
+        /// <summary>
+        /// Gets a value indicating whether the collection is read-only.
+        /// </summary>
         public bool IsReadOnly => _isReadOnly;
 
+        /// <summary>
+        /// Makes the collection readonly
+        /// </summary>
+        /// <returns>
+        /// The collection after being modified to be read-only.
+        /// </returns>
         public EnumeratedCollection<TKey, TValue> MakeReadonly()
         {
             _isReadOnly = true;
             return this;
         }
 
+        /// <summary>
+        /// Copies the elements of the collection to a new array and returns the copy.
+        /// </summary>
+        /// <returns>
+        /// The newly created array.
+        /// </returns>
         public TValue[] ToArray() => _values.ToArray();
 
+
+        /// <summary>
+        /// Gets an enumerator that will iterate through the collection.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerator<TValue> GetEnumerator()
         {
             foreach (var value in _values)
@@ -87,6 +145,11 @@ namespace AP.Collections
             }
         }
 
+        /// <summary>
+        /// If the array contains the value, it will return the index of first occurrence of the value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public int IndexOf(TValue value)
         {
             if (value == null)
@@ -103,6 +166,18 @@ namespace AP.Collections
             return -1;
         }
 
+        /// <summary>
+        /// Tries to get the key for a particular value.
+        /// </summary>
+        /// <param name="value">
+        /// The value to get the key for.
+        /// </param>
+        /// <param name="key">
+        /// Returns the key if the value is found.
+        /// </param>
+        /// <returns>
+        /// Returns true if the value is found, otherwise false.
+        /// </returns>
         public bool TryGetKeyOf(TValue value, out TKey key)
         {
             var index = IndexOf(value);
@@ -118,6 +193,12 @@ namespace AP.Collections
             }
         }
 
+        /// <summary>
+        /// Gets an enumerator that will iterate through the collection.
+        /// </summary>
+        /// <returns>
+        /// The enumerator.
+        /// </returns>
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
